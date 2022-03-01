a64ea66ff1202ea8e978800fdf1980b08cd0cfa0
adamjmcgrath <adam.mcgrath@auth0.com>
2022-03-01 10:32:04 +0000

    wip

--- src/auth0-provider-with-history.tsx
+++ src/auth0-provider-with-history.tsx
@@ -1,22 +1,26 @@
 import { AppState, Auth0Provider } from "@auth0/auth0-react";
-import React, { PropsWithChildren } from "react";
-import { useNavigate } from "react-router-dom";
+import React, {PropsWithChildren, useCallback, useState} from "react";
+import { Navigate } from "react-router-dom";
 
 export const Auth0ProviderWithHistory = ({
   children,
 }: PropsWithChildren<any>): JSX.Element | null => {
-  const navigate = useNavigate();
+  const [navigateTo, setNavigateTo] = useState<string>('')
 
   const domain: string | undefined = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId: string | undefined = process.env.REACT_APP_AUTH0_CLIENT_ID;
   const audience: string | undefined = process.env.REACT_APP_AUTH0_AUDIENCE;
 
-  const onRedirectCallback = (appState: AppState) => {
-    navigate(appState?.returnTo || window.location.pathname);
-  };
+  const onRedirectCallback = useCallback((appState: AppState) => {
+    setNavigateTo(appState?.returnTo || window.location.pathname);
+  }, [setNavigateTo]);
 
   if (!(domain && clientId && audience)) {
     return null;
+  }
+
+  if (navigateTo) {
+    return <Navigate to={navigateTo}/>
   }
 
   return (
