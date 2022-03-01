c5ecf2f62e555abff60d03333fc9caf68f38adba
adamjmcgrath <adam.mcgrath@auth0.com>
2022-03-01 09:55:28 +0000

    wip

--- src/components/protected-route.tsx
+++ src/components/protected-route.tsx
@@ -1,17 +1,21 @@
-import { withAuthenticationRequired } from "@auth0/auth0-react";
-import React, { ComponentType } from "react";
-import { Loader } from "./loader";
+import React, { ComponentType, FC } from "react";
 
 interface ProtectedRouteProps {
   component: ComponentType;
 }
 
+const withAuthenticationRequired = (
+  Component: ComponentType
+): FC => {
+  return function WithAuthenticationRequired(): JSX.Element {
+    return <Component />;
+  };
+};
+
 export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
   component,
 }) => {
-  const Component = withAuthenticationRequired(component, {
-    onRedirecting: () => <Loader />,
-  });
+  const Component = withAuthenticationRequired(component);
 
   return <Component />;
 };
